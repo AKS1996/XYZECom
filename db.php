d8c6469bc9b85d48aae9352e97e005c60410b924
AKS1996 <sushsilsharma.ss451@gmail.com>
2017-11-12 19:04:46 +0530

    changed DB

--- db.php
+++ db.php
@@ -5,27 +5,17 @@ if(!$_ENV["VCAP_SERVICES"]){ //local dev
     $mysql_username = "root";
     $mysql_password = "";
     $mysql_database = "test";
-} else { //running in Bluemix
-    $vcap_services = json_decode($_ENV["VCAP_SERVICES" ]);
-    if($vcap_services->{'mysql-5.5'}){ //if "mysql" db service is bound to this application
-        $db = $vcap_services->{'mysql-5.5'}[0]->credentials;
-    }
-    else if($vcap_services->{'cleardb'}){ //if cleardb mysql db service is bound to this application
-        $db = $vcap_services->{'cleardb'}[0]->credentials;
-    }
-    else {
-        echo "Error: No suitable MySQL database bound to the application. <br>";
-        die();
-    }
-    $mysql_database = $db->name;
-    $mysql_port=$db->port;
-    $mysql_server_name =$db->hostname . ':' . $db->port;
-    $mysql_username = $db->username;
-    $mysql_password = $db->password;
+} else {
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    echo "shit";
+    $server =$url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
 }
 //echo "Debug: " . $mysql_server_name . " " .  $mysql_username . " " .  $mysql_password . "\n";
 
-$mysqli = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
+$mysqli = new mysqli($server, $username, $password, $db);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     die();
