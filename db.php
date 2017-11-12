4b08c87dc9359f0dc96d7d90d99fdb9c792bf3c8
AKS1996 <sushsilsharma.ss451@gmail.com>
2017-11-12 18:40:24 +0530

    minor changes for heroku

--- db.php
+++ db.php
@@ -9,18 +9,18 @@ if(!$_ENV["VCAP_SERVICES"]){ //local dev
     $vcap_services = json_decode($_ENV["VCAP_SERVICES" ]);
     if($vcap_services->{'mysql-5.5'}){ //if "mysql" db service is bound to this application
         $db = $vcap_services->{'mysql-5.5'}[0]->credentials;
-    } 
+    }
     else if($vcap_services->{'cleardb'}){ //if cleardb mysql db service is bound to this application
         $db = $vcap_services->{'cleardb'}[0]->credentials;
-    } 
-    else { 
+    }
+    else {
         echo "Error: No suitable MySQL database bound to the application. <br>";
         die();
     }
     $mysql_database = $db->name;
     $mysql_port=$db->port;
     $mysql_server_name =$db->hostname . ':' . $db->port;
-    $mysql_username = $db->username; 
+    $mysql_username = $db->username;
     $mysql_password = $db->password;
 }
 //echo "Debug: " . $mysql_server_name . " " .  $mysql_username . " " .  $mysql_password . "\n";
