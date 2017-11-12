2be565782073f4c874eaca0f69bf56b61e078c65
AKS1996 <sushsilsharma.ss451@gmail.com>
2017-11-12 19:57:37 +0530

    last error left

--- web/db.php
+++ web/db.php
@@ -1,8 +1,6 @@
 <?php
 
-echo getenv("CLEARDB_DATABASE_URL");
 if(!getenv("CLEARDB_DATABASE_URL")){ //local dev
-    echo "shot";
     $mysql_server_name = "127.0.0.1:3306";
     $mysql_username = "root";
     $mysql_password = "";
@@ -15,14 +13,14 @@ if(!getenv("CLEARDB_DATABASE_URL")){ //local dev
     $password = $url["pass"];
     $db = substr($url["path"], 1);
 }
-//echo "Debug: " . $mysql_server_name . " " .  $mysql_username . " " .  $mysql_password . "\n";
+echo "Debug: " . $mysql_server_name . " " .  $mysql_username . " " .  $mysql_password . "\n";
 
 $mysqli = new mysqli($server, $username, $password, $db);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     die();
 }else{
-//	echo "Connection working well";
+	echo "Connection working well";
 }
 
 ?>
